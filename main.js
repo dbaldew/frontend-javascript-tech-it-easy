b3d51d9063170a11f64a77cbc6da38dc8eefe47c
dbaldew <dbaldew@gmail.com>
2021-07-21 13:05:53 +0200

    update

--- main.js
+++ main.js
@@ -161,3 +161,125 @@ const inventory = [
     sold: 8,
   },
 ];
+
+// Opdracht 1a: Hoeveel exemplaren moeten we nog verkopen? Schrijf een functie die dit berekent.
+// aantal nog te verkopen units is gelijk aan (originalStock - sold) per type en dan totaal berekenen.
+const typesToSell = inventory.map((unit) => {
+  return unit.originalStock - unit.sold
+})
+console.log(typesToSell)
+
+const totalUnitsToSell = typesToSell.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+})
+console.log(totalUnitsToSell);
+
+
+//Opdracht 1b: Zorg ervoor dat dit aantal in het rood wordt weergegeven op de pagina => css
+
+//Opdracht 2a: Gebruik een array-methode om een array te maken met alle tv-type namen.
+tvTypes = inventory.map((typeName) => {
+  return typeName.type;
+})
+console.log(tvTypes)
+
+// Opdracht 2b: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht zijn.
+tvSoldOut = inventory.filter((count) => {
+  if (count.originalStock - count.sold === 0){
+    return count;
+  }
+})
+console.log(tvSoldOut);
+
+// Opdracht 2c: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight beschikken.
+tvAmbilight = inventory.filter((light) => {
+  if (light.options.ambiLight){
+    return light;
+  }
+})
+console.log(tvAmbilight);
+
+// Opdracht 2d: Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert.
+tvPriceSorted = inventory.sort((previousPrice, currentPrice) => {
+  return previousPrice.price - currentPrice.price ;
+})
+console.log(tvPriceSorted);
+
+//Opdracht 3a: Wat is onze doel-opbrengst? Bereken wat de totale opbrengst is, als we alle exemplaren van ieder type
+// zouden verkopen. Geef dit in het blauw weer op de pagina.
+profitPerType = inventory.map((unit) => {
+  return (unit.originalStock - unit.sold)* unit.price;
+})
+console.log(profitPerType);
+
+totalProfit = profitPerType.reduce((accumulator, currentValue) =>{
+  return accumulator + currentValue;
+})
+console.log(totalProfit);
+
+
+// Opdracht 3b: Hoeveel hebben we tot nu toe verdiend? Bereken hoeveel we tot nu toe verdiend hebben met het aantal verkochte tv's. Geef dit weer in het groen weer op de pagina
+profitUnitsSold = inventory.map ((unit) => {
+  return (unit.sold * unit.price);
+})
+console.log(profitUnitsSold);
+
+
+//Opdracht 4
+// Geef de type-namen van twee tv's weer op de pagina. Welke tv's dat precies zijn, maakt niet zoveel uit.
+// Voor nu betekent dit dat je het appenden van de nodes twee keer moet uitschrijven, dat is niet erg
+
+
+//Opdracht 5
+// We gaan één van de twee tv's van de vorige opdracht weergeven in het volgende format:
+//
+// Philips 43PUS6504/12 - 4K TV
+// €379,-
+// 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm)
+
+// NIKKEI NH3216SMART - HD smart TV
+// €159,-
+// 32 inch (81 cm)
+
+
+// Opdracht 5a: Zorg ervoor dat er een string wordt gegenereerd voor de naam van een tv. Maak een functie die één
+// tv-object als parameter verwacht en de naam op de volgende manier samenvoegt:
+// [merk] [type] - [naam] zoals Philips 43PUS6504/12 - 4K TV of NIKKEI NH3216SMART - HD smart TV.
+// Zorg ervoor dat je deze functie voor iedere tv zou kunnen gebruiken.
+
+
+// Opdracht 5b: Zorg ervoor dat de prijs van een tv netjes geformat wordt.
+// Maak een functie die één tv-prijs als parameter verwacht (zoals 379) en daar de volgende string
+// van maakt: €379,-. Zorg ervoor dat je deze functie voor iedere tv zou kunnen gebruiken.
+
+
+// Opdracht 5c: Zorg ervoor dat er een string wordt gegenereerd voor alle beschikbare schermgroottes van één tv
+// in zowel inches als cm Maak een functie die één screen-sizes array verwacht en de groottes op de volgende
+// manier samenvoegt:
+// [schermgrootte] inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm) etc.
+// Dus een input van [32] geeft 32 inch (81 cm)
+// en een input van [43, 50, 55, 58] geeft 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm).
+// Zorg ervoor dat je deze functie voor iedere tv zou kunnen gebruiken, zowel voor tv's met maar één schermgrootte
+// als met tientallen schermgroottes.
+
+
+
+// Opdracht 5d: Zorg ervoor de informatie van één van de twee tv's zoals het voorbeeld wordt weergegeven op de pagina.
+// Gebruik hiervoor de functies die je hebt gemaakt in opdracht 5a, 5b en 5c.
+
+
+
+// Opdracht 5e: Schrijf een functie die ALLE tv's weergeeft op de pagina zoals in het voorbeeld.
+// Dit wil je natuurlijk niet acht keer opnieuw schrijven, want nu zijn het 8 tv's,
+// maar in de toekomst misschien wel 200! Gebruik in deze functie de voorgaande functies die je hebt geschreven,
+// om onderdelen van de data te formatten.
+// Deze "tv-generator-functie" verwacht één parameter: de volledige array met tv-objecten. Vergeet 'm niet aan te roepen!
+
+
+
+// Bonusopdracht
+// Maak drie knoppen op de pagina: Sorteer op prijs, AmbiLight TV's en Uitverkochte exemplaren.
+// Gebruik de code die je in opdracht 2b, 2c en 2d hebt gemaakt en schrijf dit om naar functies
+// zodat je ze kunt aanroepen op het moment dat de buttons geklikt worden.
+// Zorg ervoor dat de functies de uitkomsten in de de console loggen als de gebruiker op de bijbehorende knop klikt.
+// Tip: Google eens naar het onclick event van buttons!
